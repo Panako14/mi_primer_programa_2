c8a60f89c32d7142b4157701588325932a018296
Panako14 <ivantorgim@gmal.com>
2018-04-26 10:37:04 +0200

    Adivina un número 5 intentos

---
+++ adivina_un_numero.py
@@ -0,0 +1,27 @@
+number_to_guess = 2
+user_number = int(input("Adivina un número: "))
+
+if user_number == number_to_guess:
+    print("Has ganado")
+else:
+    user_number = int(input("No es correcto, elige otro número: "))
+
+    if user_number == number_to_guess:
+        print("Has ganado")
+    else:
+        user_number = int(input("No es correcto, elige otro número: "))
+
+        if user_number == number_to_guess:
+            print("Has ganado")
+        else:
+            user_number = int(input("No es correcto, elige otro número: "))
+
+            if user_number == number_to_guess:
+                print("Has ganado")
+            else:
+                user_number = int(input("No es correcto, elige otro número: " ))
+
+                if user_number == number_to_guess:
+                    print("Has ganado")
+                else:
+                    print("Has perdido")

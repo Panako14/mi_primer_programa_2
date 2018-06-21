284e3550bdcb61a40425ee243fd83ee0467144a5
Panako14 <ivantorgim@gmal.com>
2018-06-21 17:24:32 +0200

    Adivina un numero 2

---
+++ adivina_un_numero_2.py
@@ -0,0 +1,7 @@
+numero_a_adivinar = input('Elige un número: ')
+numero_elegido = input('Adivina el número: ')
+
+while numero_a_adivinar != numero_elegido:
+    numero_elegido = input('Adivina el número: ')
+
+print('Has acertado.')

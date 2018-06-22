4f470a3a50506f0721f039c0547e5aacb2fe2e8e
Panako14 <ivantorgim@gmal.com>
2018-06-22 16:27:27 +0200

    Conversor de temperatura Farenheit a Celsius.

---
+++ conversor_farenheit_a_celsius.py
@@ -0,0 +1,6 @@
+print('-Conversor de grados Farenheit a Celsius-')
+farenheit = int(input('Dime los grados en Farenheit: '))
+
+celsius = (farenheit - 32) /1.8
+
+print('Son {} grados Celsius.'.format(celsius))
